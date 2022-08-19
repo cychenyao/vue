d78554b5b7454f7a8e71c83668bde4eaf1803de4
cychenyao <46012528+cychenyao@users.noreply.github.com>
2022-08-19 18:09:49 +0800

    Add files via upload

---
+++ basics-chapter2-04-双向数据绑定/app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            counter: 0,
+            name: ''
+        }
+    },
+    methods:{
+        add(index){
+            this.counter = this.counter+index
+        },
+        reduce(index){
+            this.counter = this.counter - index
+        },
+        setName(event){
+            this.name= event.target.value
+        },
+        submitForm(){
+            alert("提交表单！")
+        },
+        clearName(){
+            this.name= ''
+        }
+    }
+})
+app.mount('body')
