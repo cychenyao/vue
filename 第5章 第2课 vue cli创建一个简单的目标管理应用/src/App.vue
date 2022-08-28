0c52411b8bc87190193352c6479738a9bd9fe3fb
cychenyao <46012528+cychenyao@users.noreply.github.com>
2022-08-28 18:40:09 +0800

    Add files via upload

---
+++ 第5章 第2课 vue cli创建一个简单的目标管理应用/src/App.vue
@@ -0,0 +1,58 @@
+
+<template>
+  <section id="events">
+     <h2>所有待做</h2>
+      <ul>
+        <li v-for="goal in goals" :key="goal">
+          <my-goal :goal="goal"></my-goal>
+        </li>
+      </ul>
+  </section>
+</template>
+<script>
+
+export default {
+  data(){
+    return {
+      goals: [
+                {
+                    id:1,
+                    title: '完成vue课程',
+                    description: '每天学一点，争取一个月完成vue课程'
+                },
+                {
+                    id:2,
+                    title: '每天运动1个小时',
+                    description: '每天运动1个小时，5公里'
+                },
+                {
+                    id:3,
+                    title: '早睡早起',
+                    description: '晚上11点睡觉，早上7点起床'
+                }
+            ],
+          showDetail: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+#events {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#events h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #846181;
+  margin: 0 0 1rem 0;
+}
+
+</style>
---
+++ 第5章 第2课 vue cli创建一个简单的目标管理应用/src/components/MyGoal.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <section>
+     <p @click="showDetail = !showDetail">{{goal.title}} -- {{index}}</p>
+    <p v-if="showDetail" style="background:peru">{{goal.description}}</p>
+</section>
+</template>
+
+<script>
+
+export default {
+  props: ['goal'],
+  data(){
+    return {
+        showDetail:false
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 1.25rem;
+  font-weight: bold;
+  border: 1px solid #846181;
+  background-color: #846181;
+  color: white;
+  padding: 0.5rem;
+  border-radius: 25px;
+}
+
+</style>
